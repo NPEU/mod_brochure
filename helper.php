76f97390432aa7bc69af46afeb113da10e281dda
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-01-30 12:32:18 +0000

    Added html_id filter, replaced str_replace fitlter with regex filter

--- helper.php
+++ helper.php
@@ -37,10 +37,10 @@ class ModBrochureHelper
             trigger_error('Function \'csvarray\' expects argument 1 to be an string', E_USER_ERROR);
             return false;
         }
-        
+
         // Normalise new lines:
         trim(preg_replace('~\r\n?~', "\n", $csv));
-        
+
         // Remove breaks from within quotes:
         if (preg_match_all('/"[^"]+"/', $csv, $matches)) {
             foreach($matches[0] as $match) {
@@ -109,7 +109,81 @@ class ModBrochureHelper
         }
         return $data;
     }
-    
+
+    /**
+     * Creates an HTML-friendly string for use in id's
+     *
+     * @param string $text
+     * @return string
+     * @access public
+     */
+    public static function htmlID($text)
+    {
+        if (!is_string($text)) {
+            trigger_error('Function \'html_id\' expects argument 1 to be an string', E_USER_ERROR);
+            return false;
+        }
+        $return = strtolower(trim(preg_replace('/\s+/', '-', self::stripPunctuation($text))));
+        return $return;
+    }
+
+
+    /**
+     * Strips punctuation from a string
+     *
+     * @param string $text
+     * @return string
+     * @access public
+     */
+    public static function stripPunctuation($text)
+    {
+        if (!is_string($text)) {
+            trigger_error('Function \'strip_punctuation\' expects argument 1 to be an string', E_USER_ERROR);
+            return false;
+        }
+        $text = html_entity_decode($text, ENT_QUOTES);
+
+        $urlbrackets = '\[\]\(\)';
+        $urlspacebefore = ':;\'_\*%@&?!' . $urlbrackets;
+        $urlspaceafter = '\.,:;\'\-_\*@&\/\\\\\?!#' . $urlbrackets;
+        $urlall = '\.,:;\'\-_\*%@&\/\\\\\?!#' . $urlbrackets;
+
+        $specialquotes = '\'"\*<>';
+
+        $fullstop = '\x{002E}\x{FE52}\x{FF0E}';
+        $comma = '\x{002C}\x{FE50}\x{FF0C}';
+        $arabsep = '\x{066B}\x{066C}';
+        $numseparators = $fullstop . $comma . $arabsep;
+
+        $numbersign = '\x{0023}\x{FE5F}\x{FF03}';
+        $percent = '\x{066A}\x{0025}\x{066A}\x{FE6A}\x{FF05}\x{2030}\x{2031}';
+        $prime = '\x{2032}\x{2033}\x{2034}\x{2057}';
+        $nummodifiers = $numbersign . $percent . $prime;
+        $return = preg_replace(
+        array(
+            // Remove separator, control, formatting, surrogate,
+            // open/close quotes.
+            '/[\p{Z}\p{Cc}\p{Cf}\p{Cs}\p{Pi}\p{Pf}]/u',
+            // Remove other punctuation except special cases
+            '/\p{Po}(?<![' . $specialquotes .
+            $numseparators . $urlall . $nummodifiers . '])/u',
+            // Remove non-URL open/close brackets, except URL brackets.
+            '/[\p{Ps}\p{Pe}](?<![' . $urlbrackets . '])/u',
+            // Remove special quotes, dashes, connectors, number
+            // separators, and URL characters followed by a space
+            '/[' . $specialquotes . $numseparators . $urlspaceafter .
+            '\p{Pd}\p{Pc}]+((?= )|$)/u',
+            // Remove special quotes, connectors, and URL characters
+            // preceded by a space
+            '/((?<= )|^)[' . $specialquotes . $urlspacebefore . '\p{Pc}]+/u',
+            // Remove dashes preceded by a space, but not followed by a number
+            '/((?<= )|^)\p{Pd}+(?![\p{N}\p{Sc}])/u',
+            // Remove consecutive spaces
+            '/ +/',
+            ), ' ', $text);
+        $return = str_replace('/', '_', $return);
+        return str_replace("'", '', $return);
+    }
 
     /**
      * Gets a twig instance - useful as we don't have to re-declare customisations each time.
@@ -121,7 +195,7 @@ class ModBrochureHelper
     {
         $loader = new Twig_Loader_Array($tpls);
         $twig   = new Twig_Environment($loader);
-        
+
         // Add markdown filter:
         $md_filter = new Twig_SimpleFilter('md', function ($string) {
             $new_string = '';
@@ -129,10 +203,10 @@ class ModBrochureHelper
             $new_string = Markdown::defaultTransform($string);
             return $new_string;
         });
-        
+
         $twig->addFilter($md_filter);
         // Use like {{ var|md|raw }}
-        
+
         // Add pad filter:
         $pad_filter = new Twig_SimpleFilter('pad', function ($string, $length, $pad = ' ', $type = 'right') {
             $new_string = '';
@@ -150,21 +224,32 @@ class ModBrochureHelper
             $length = (int) $length;
             $pad    = (string) $pad;
             $new_string = str_pad($string, $length, $pad, $type);
-            
+
             return $new_string;
         });
         $twig->addFilter($pad_filter);
-        
-        // Add str_replace filter:
-        $str_filter = new Twig_SimpleFilter('str_replace', function ($string, $search = '', $replace = '') {
+
+        // Add regex_replace filter:
+        $regex_replace_filter = new Twig_SimpleFilter('regex_replace', function ($string, $search = '', $replace = '') {
             $new_string = '';
- 
-            $new_string = str_replace($search, $replace, $string);
-            
+
+            $new_string = preg_replace($search, $replace, $string);
+
             return $new_string;
         });
-        $twig->addFilter($str_filter);
-        
+        $twig->addFilter($regex_replace_filter);
+
+        // Add html_id filter:
+        $html_id_filter = new Twig_SimpleFilter('html_id', function ($string) {
+            $new_string = '';
+
+            $new_string = self::htmlID($string);
+
+            return $new_string;
+        });
+        $twig->addFilter($html_id_filter);
+
+
         return $twig;
     }
 }
