e833e1d4be52ac720452268b81c0eae3826cc08b
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-03-13 16:36:49 +0000

    Added pane balance option

--- tmpl/default.php
+++ tmpl/default.php
@@ -16,11 +16,27 @@ $module_path = str_replace($_SERVER['DOCUMENT_ROOT'], '', dirname(__DIR__));
 if ($load_assets) {
     $css_file = $module_path . '/assets/brochure.css';
     $doc->addStyleSheet($css_file);
+    
+    #$css_file_2 = $module_path . '/assets/ff-widths.min.css';
+    #$doc->addStyleSheet($css_file_2);
 }
 
+$pane_classes = array('brochure__panes');
+
+if ($single_pane_type !== false) {
+    $pane_classes[] = 'brochure__panes--single';
+} else {
+    $pane_classes[] = 'brochure__panes--multiple';    
+    
+    if (!empty($pane_balance)) {
+        $pane_classes[] = 'brochure__panes--multiple--' . $pane_balance; 
+    }
+}
+
+$pane_classes = implode('  ', $pane_classes);
 ?>
 <div class="brochure<?php echo $moduleclass_sfx; ?>">    
-    <div class="brochure__panes<?php if ($single_pane_type !== false): ?>  brochure__panes--single<?php else: ?>  brochure__panes--multiple<?php endif; ?>">
+    <div class="<?php echo $pane_classes; ?>">
         <?php if ($single_pane_type !== false): ?>
         <div class="brochure__panes--single">
             <?php #echo $single_pane_content; 
